69ba30ce26ae924329ddc03afd22a65c56df2779
Lu-Kakoy <sasaharusin@gmail.com>
2024-11-13 16:37:11 +0800

    Update mail1.py

--- 7.py
+++ 7.py
@@ -16,62 +16,46 @@
 
 
 
-# email2 = email1.count
 
-# test1 = 'st@roka'
 
-# test2 = test1.find('@')
-# print(test2)
-
-def fun1(sender):
+def fun1(sender):                                                                                                       # Проверка на корректность e-mail отправителя
     if sender.find('@') == -1:
-        print('все  плохо1')
+        print('все  плохо1')        #<<<флаги для выявления ошибок в отдельных частях кода
         st1 = 0
     else:
-        print('все хорошо1')
+        # print('все хорошо1')
         if sender.endswith('.ru') == True:
-            print('все хорошо2')
+            # print('все хорошо2')
             st1 = 1
         elif sender.endswith('.com') == True:
-            print('все хорошо3')
+            # print('все хорошо3')
             st1 = 1
         else:
             print('почта указана не верно')
             st1 = 0
     return st1
 
-# fun1(sender)
 
-# print('''
-# 2
-# ''')
 
-# recipient = 'sender@gmail.com'
-
-def fun2(recipient):
+def fun2(recipient):                                                                                                    # Проверка на корректность e-mail получателя
     if recipient.find('@') == -1:
-        print('все плохо2')
+        # print('все плохо2')
         st2 = 0
     else:
-        print('все хорошо1')
-        if recipient.endswith('.ru') == True:
-            print('все хорошо2')
+        # print('все хорошо1')
+        if recipient.endswith('.ru'):
+            # print('все хорошо2')
             st2 = 1
-        elif recipient.endswith('.com') == True:
-            print('все хорошо3')
+        elif recipient.endswith('.com'):
+            # print('все хорошо3')
             st2 = 1
         else:
             print('почта указана не верно')
             st2 = 0
     return st2
 
-# fun2(recipient)
 
-# print('''
-# 3
-# ''')
-
-def fun3(sender, recipient):
+def fun3(sender, recipient):                                                                                            # Проверка на отправку самому себе
     if sender == recipient:
         print('нельзя отправить сообщение себе!')
         st3 = 0
@@ -80,28 +64,20 @@ def fun3(sender, recipient):
         st3 = 1
     return st3
 
-# fun3(sender, recipient)
 
-# print('''
-# 4
-# ''')
-
-def fun4(standart_sender, sender):
+def fun4(standart_sender, sender):                                                                                      # Проверка на отправителя по умолчанию
     if standart_sender == sender:
         print(f'для отправки сообщения использована стандартная почта отправителя - {standart_sender}')
     else:
         print(f'для отправки сообщения использована почта отличная от стандартной - {sender}')
 
-# fun4(standart_sender, sender)
 
 
 def start():
     choice1 = ''
-    end = ''
     mail = {}
     number_of_messages = 0
-    while choice1 != 'end':
-        choice1 = ''
+    while choice1 != 'end':         # Для завершения работы программы нужно написать "end"
         choice1 = input('''
         1 - написать письмо
         2 - посмотреть почту
@@ -111,39 +87,31 @@ def start():
             end = ''
             standart_sender = 'sender@yandex.ru'
             while end != 'end':
-                print('этап - 1')
+                # print('этап - 1')
                 # sender = 'sender@yandex.ru'
-                sender = input('введите адрес электронной почты отправителя: ')
                 # recipient = 'recipient@gmail.com'
-                recipient = input('Введите адрес электронной почты получателя: ')
                 # message1 = 'Hello World, i came for you.'
+                sender = input('введите адрес электронной почты отправителя: ')
+                recipient = input('Введите адрес электронной почты получателя: ')            # прием данных пользователя
                 message1 = input('введите сообщение: ')
-                stage1 = fun1(sender)
-
+                stage1 = fun1(sender)                                   #<<< Проверка на корректность e-mail отправителя
                 if stage1 == 1:
                     while True:
-                        print('этап - 2')
-                        stage2 = fun2(recipient)
+                        # print('этап - 2')
+                        stage2 = fun2(recipient)                         #<<< Проверка на корректность e-mail получателя
                         if stage2 == 1:
-                            print('этап - 3')
-                            stage3 = fun3(sender, recipient)
+                            # print('этап - 3')
+                            stage3 = fun3(sender, recipient)                       #<<< Проверка на отправку самому себе
                             if stage3 == 1:
-                               print('на 3 этапе все хорошо')
-                               fun4(standart_sender, sender)
-                               number_of_messages += 1
-                               message2 = (f'отправитель - {sender}: {message1} получатель - {recipient}')
+                               # print('на 3 этапе все хорошо')
+                               fun4(standart_sender, sender)                   #<<< Проверка на отправителя по умолчанию
+                               number_of_messages += 1         #<Используется для присвоения уникальных ключей для писем и их дальнейшего извлечения
+                               message2 = f'отправитель - {sender}: {message1} получатель - {recipient}'
                                mail[number_of_messages] = message2
                                end = 'end'
                                break
                             else:
                                 break
-
-                        else:
-                            # print('введите корректный адрес получателя')
-                            recipient = input('Введите корректный адрес электронной почты получателя: ')
-                else:
-                    # print('введите корректный адрес отправителя')
-                    sender = input('введите корректный адрес электронной почты отправителя: ')
         elif choice1 == '2':
             choice2 = int(input(f'''у вас {number_of_messages} писем, какое хотите прочесть?
 выбор: '''))
@@ -152,46 +120,3 @@ def start():
 
 start()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Codewars, Chekio
